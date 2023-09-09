cbb3e5f5aa18f9b77f96f41cfc3457dcddeb70e4
igor <neitron_h19@mail.ru>
2023-09-09 19:31:18 +0300

    fix filter

--- js/filter.js
+++ js/filter.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
     document.querySelector('.filter_block').addEventListener('click', (e) => {
         e.target.closest('.filter').classList.toggle('open-filter');
-        e.target.closest('.page').classList.toggle('shadow');
     })
 })
