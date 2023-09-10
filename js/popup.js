c358540f186c82ee38197da5ed8cb531fc5da6d9
igor <neitron_h19@mail.ru>
2023-09-10 15:56:46 +0300

    create castomPopup

---
+++ js/popup.js
@@ -0,0 +1,24 @@
+function castomPopup(selector, html, close = true){
+    document.querySelector(selector).addEventListener('click', () => {
+        const shadow = document.createElement('div'),
+        popup = document.createElement('div');
+
+        shadow.classList.add('shadow-popup');
+        popup.classList.add('window-popup');
+        popup.innerHTML  = close ? '<div class="close-popup"></div>'+html : html;
+
+        
+
+        document.querySelector('body').append(shadow);
+        document.querySelector('body').append(popup);
+
+        document.querySelector('.close-popup').addEventListener('click', () => {
+            shadow.remove();
+            popup.remove();
+        })
+        shadow.addEventListener('click', () => {
+            shadow.remove();
+            popup.remove();
+        })
+    })
+}
